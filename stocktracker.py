64f90841c195d69a2d7ea5fa8e45b3721be0aea3
mayokunl <153923029+mayokunl@users.noreply.github.com>
2025-06-30 16:09:37 -0500

    first commit

--- stocktracker.py
+++ stocktracker.py
@@ -1,1 +1,17 @@
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
+import os
+
+API_KEY = os.environ.get("API_KEY")
+
+stock = input("Enter Stock Name to fetch data : ")
+
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock}&apikey=WARSFA60NM8N6R8Q'
+r = requests.get(url)
+data = r.json().get("Time Series (Daily)", {})
+
+df = pd.DataFrame(data)
+
+print(df)
 
