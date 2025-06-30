408afdb7e244b4b10bf1b44f014790bad7227591
mayokunl <153923029+mayokunl@users.noreply.github.com>
2025-06-30 16:30:23 -0500

    API was still in there

--- stocktracker.py
+++ stocktracker.py
@@ -7,7 +7,7 @@ API_KEY = os.environ.get("API_KEY")
 
 stock = input("Enter Stock Name to fetch data : ")
 
-url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock}&apikey=WARSFA60NM8N6R8Q'
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock}&apikey={API_KEY}'
 r = requests.get(url)
 data = r.json().get("Time Series (Daily)", {})
 
